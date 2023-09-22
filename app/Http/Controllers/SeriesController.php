cd16253e42d6b8a74c8b0bf1bd55cc9dfc560fb3
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-22 16:06:29 -0300

    Criando HTML

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -7,20 +7,12 @@ use Illuminate\Http\Request;
 class SeriesController extends Controller
 {
     public function index(Request $request) {
-
-        return redirect('https://google.com');
         $series = [
             'Punisher',
             'Lost',
             'Grey\'s Anatomy'
         ];
 
-        $html = '<ul>';
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= '</ul>';
-
-        return $html;
+        return view('listar-series', compact('series'));
     }
 }
