b65e83245a4bab85e53d8e3530ec87abb39e19df
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-25 13:45:10 -0300

    Estilizando as Views

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,4 @@ const mix = require('laravel-mix')
 // Guia de migração para o Vite: https://github.com/laravel/vite-plugin/blob/main/UPGRADE.md#migrating-from-laravel-mix-to-vite
 
 mix.sass('resources/css/app.scss', 'public/css')
+
