1d47d3923e8dc1b1cd723e14140a8a7125346efb
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-26 08:06:52 -0300

    Session e Flash Messages

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,10 +8,11 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $series = Serie::query()->orderBy('nome')->get();
+        $mensagemSucesso = session('mensagem.sucesso');
 
-        return view('series.index')->with('series', $series);
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create() {
@@ -21,11 +22,14 @@ class SeriesController extends Controller
     public function store(Request $request) {
         // Mass Assignment
         Serie::create($request->all());
+
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
         return to_route('series.index');
     }
 
     public function destroy(Request $request) {
         Serie::destroy([$request->series]);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
         return to_route('series.index');
     }
 }
