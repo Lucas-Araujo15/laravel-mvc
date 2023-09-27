b19deda5906f5b8d71cab07cf28aabf4cae07496
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-27 12:32:24 -0300

    Temporadas e episódios

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,3 +1,36 @@
 <x-layout title="Nova Série">
-    <x-form :action="route('series.store')" :update="false"/>
+    <form action="{{ route('series.store') }}" method="POST">
+        @csrf
+        <div class="row mb-3">
+            <div class="col-8">
+                <label for="nome" class="form-label">Nome:</label>
+                <input
+                    autofocus
+                    type="text"
+                    name="nome"
+                    id="nome"
+                    class="form-control"
+                    value="{{ old('nome') }}">
+            </div>
+            <div class="col-2">
+                <label for="seasonsQty" class="form-label">Nº de temporadas:</label>
+                <input
+                    type="text"
+                    name="seasonsQty"
+                    id="seasonsQty"
+                    class="form-control"
+                    value="{{ old('seasonsQty') }}">
+            </div>
+            <div class="col-2">
+                <label for="episodesPerSeason" class="form-label">EPs por temporadas:</label>
+                <input
+                    type="text"
+                    name="episodesPerSeason"
+                    id="episodesPerSeason"
+                    class="form-control"
+                    value="{{ old('episodesPerSeason') }}">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
 </x-layout>
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,3 @@
-<x-layout title="Editar série {{ $series->nome }}">
+<x-layout title="Editar série '{!! $series->nome !!}'">
     <x-form :action="route('series.update', $series)" :nome="$series->nome" :update="true" />
 </x-layout>
