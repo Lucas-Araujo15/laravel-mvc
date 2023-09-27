d61a5d891da97f83da75df4aca79007f4d6749fe
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-27 12:05:29 -0300

    Recriando o banco

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Serie;
+use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request) {
-        $series = Serie::all();
+        $series = Series::all();
         $mensagemSucesso = session('mensagem.sucesso');
 
         return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
@@ -22,20 +22,20 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request) {
         // Mass Assignment
-        $serie = Serie::create($request->all());
+        $serie = Series::create($request->all());
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' foi adicionada com sucesso");
     }
 
-    public function destroy(Serie $series) {
+    public function destroy(Series $series) {
         $series->delete();
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' foi removida com sucesso");
     }
 
-    public function edit(Serie $series) {
+    public function edit(Series $series) {
         return view('series.edit')->with('series', $series);
     }
 
-    public function update(SeriesFormRequest $request, Serie $series) {
+    public function update(SeriesFormRequest $request, Series $series) {
         $series->nome = $request->nome;
         $series->save();
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' foi alterada com sucesso");
